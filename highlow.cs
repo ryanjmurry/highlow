6caf5d239a28a00d26f4109282d58e8ad8137ca2
Ryan Murry and Renee Sarley <ryan@>
2018-06-25 15:50:40 -0700

    move logic from main to HighLow class

--- highlow.cs
+++ highlow.cs
@@ -1,41 +1,58 @@
 using System;
 using System.Collections.Generic;
 
+class HighLow
+{
+  private int _guess;
+  private string _userResponse;
+  private List<int> _range = new List<int>();
+
+  public void FillList()
+  {
+    for(int i = 1; i < 101; ++i)
+    {
+      _range.Add(i);
+    }
+  }
+
+  public void GuessingGame()
+  {
+    FillList();
+    while(true)
+    {
+      _guess = _range[(_range.Count - 1) / 2];
+      Console.WriteLine(_guess);
+      Console.WriteLine(_range.Count);
+      Console.WriteLine("Is your number higher or lower than {0}? (Lower, Higher, Correct)", _guess);
+      _userResponse = Console.ReadLine();
+      if (_range.Count - 1 == 1)
+      {
+        break;
+      }
+      else if (_userResponse == "Higher")
+      {
+        _range.RemoveRange(0, (_range.Count - 1) / 2);
+      }
+      else if (_userResponse == "Lower")
+      {
+        _range.RemoveRange((_range.Count - 1) / 2, _range.Count / 2);
+      }
+      // else if (_userResponse == "Correct")
+      // {
+      //   break;
+      // }
+    }
+    Console.WriteLine("Your number is {0}", _guess);
+  }
+}
+
+
 class Program
 {
   public static void Main()
   {
-    int guess;
-    string userResponse;
-    List<int> range = new List<int>();
-    for(int i = 1; i < 101; ++i)
-    {
-      range.Add(i);
-    }
-
-    while(true)
-    {
-      guess = range[(range.Count - 1) / 2];
-      Console.WriteLine(guess);
-      Console.WriteLine(range.Count);
-      Console.WriteLine("Is your number higher or lower than {0}? (Lower, Higher, Correct)", guess);
-      userResponse = Console.ReadLine();
-      if (userResponse == "Higher")
-      {
-        range.RemoveRange(0, (range.Count - 1) / 2);
-      }
-      else if (userResponse == "Lower")
-      {
-        range.RemoveRange((range.Count - 1) / 2, range.Count / 2);
-      }
-      else if (userResponse == "Correct")
-      {
-        break;
-      }
-
-    }
-
-    Console.WriteLine("Your number is {0}", guess);
+    HighLow newGame = new HighLow();
+    newGame.GuessingGame();
   }
 
 }
