892fb45b7c3b08d80fc8f2df82f37940e0342fd8
Ryan Murry and Renee Sarley <renee@>
2018-06-25 16:34:03 -0700

    add human guessing game method

--- highlow.cs
+++ highlow.cs
@@ -4,18 +4,69 @@ using System.Collections.Generic;
 class HighLow
 {
   private int _guess;
+  private int _humanGuess;
+  private int _computerNumber;
   private string _userResponse;
   private List<int> _range = new List<int>();
 
+  public void WhichGame()
+  {
+    Console.WriteLine("Press 1 to have the computer guess your secret number.");
+    Console.WriteLine("Press 2 to guess the computer's secret number.");
+    string choice = Console.ReadLine();
+    if (choice == "1")
+    {
+      ComputerGuessingGame();
+    }
+    else if (choice == "2")
+    {
+      HumanGuessingGame();
+    }
+    else
+    {
+      Console.WriteLine("Please enter a valid option");
+      WhichGame();
+    }
+
+  }
+
   public void FillList()
   {
-    for(int i = 1; i < 101; ++i)
+    for(int i = 1; i < 102; ++i)
     {
       _range.Add(i);
     }
   }
 
-  public void GuessingGame()
+  public void HumanGuessingGame()
+  {
+    FillList();
+    Random rnd = new Random();
+    _computerNumber = rnd.Next(1, 101);
+    Console.WriteLine(_computerNumber);
+    while(true)
+    {
+      Console.WriteLine("What do you think the secret number is?");
+      string stringHumanGuess = Console.ReadLine();
+      _humanGuess = int.Parse(stringHumanGuess);
+
+      if (_humanGuess < _computerNumber)
+      {
+        Console.WriteLine("Your guess is TOO LOW");
+      }
+      else if (_humanGuess > _computerNumber)
+      {
+        Console.WriteLine("Your guess is TOO HIGH");
+      }
+      else if (_humanGuess == _computerNumber)
+      {
+        Console.WriteLine("THAT IS THE NUMBER");
+        break;
+      }
+    }
+  }
+
+  public void ComputerGuessingGame()
   {
     FillList();
     while(true)
@@ -52,7 +103,12 @@ class Program
   public static void Main()
   {
     HighLow newGame = new HighLow();
-    newGame.GuessingGame();
+    newGame.WhichGame();
+    Console.WriteLine("Play Again? (Y/N)");
+    string again = Console.ReadLine();
+    if (again == "Y")
+    {
+      Main();
+    }
   }
-
 }
